13ff9421ce71e980f29a3330086eb7338ecd8530
Napermial <hatvani98@freemail.hu>
2019-08-13 14:31:08 +0200

    database dao skeleton implemented

---
+++ src/main/java/com/codecool/shop/dao/implementationWIthJDBC/CartDaoJdbc.java
@@ -0,0 +1,4 @@
+package com.codecool.shop.dao.implementationWIthJDBC;
+
+public class CartDaoJdbc {
+}
---
+++ src/main/java/com/codecool/shop/dao/implementationWIthJDBC/DatabaseAccess.java
@@ -0,0 +1,26 @@
+package com.codecool.shop.dao.implementationWIthJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseAccess {
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/webshop";
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
+    private final Connection con = getConnection();
+
+    private static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    DATABASE,
+                    DB_USER,
+                    DB_PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("ERROR: Connection error.");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+

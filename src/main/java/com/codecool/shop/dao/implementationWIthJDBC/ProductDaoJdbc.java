1db156076dff04aad392688cd977051bdba7f9ab
Napermial <hatvani98@freemail.hu>
2019-08-14 11:36:28 +0200

    product dao getall, getby
    needs constructor

--- src/main/java/com/codecool/shop/dao/implementationWIthJDBC/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementationWIthJDBC/ProductDaoJdbc.java
@@ -9,10 +9,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDaoJdbc extends DatabaseAccess implements ProductDao  {
-    Connection con = getConnection();
+    private Connection con = getConnection();
 
     @Override
     public void add(Product product) {
@@ -32,31 +33,71 @@ public class ProductDaoJdbc extends DatabaseAccess implements ProductDao  {
     @Override
     public List<Product> getAll() throws SQLException {
         PreparedStatement preparedStatement = con.prepareStatement("" +
-                "SELECT id, name, price, currency, description, pc.name," +
+                "SELECT product.name, price, currency, product.description, pc.name," +
                 "pc.department, pc.description, s.name, s.description FROM product " +
                 "INNER JOIN product_category pc on product.category_id = pc.id " +
                 "INNER JOIN supplier s on product.supplier_id = s.id");
         ResultSet rs =  preparedStatement.executeQuery();
+        List<Product> products = new ArrayList<>();
         while (rs.next()){
             Product product = new Product(
-                    rs.getString("name"),
-                    rs.getFloat("price"),
-                    rs.getString("currency"),
-                    rs.getString("description"),
-                    new ProductCategory(rs.getString("pc.name"),
-                            rs.getString("pc.department"),rs.getString("pc.description")  ),
-                    new Supplier(rs.getString("s.name"), rs.getString("s.description")));
+                    rs.getString(1),
+                    rs.getFloat(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    new ProductCategory(rs.getString(5),
+                            rs.getString(6),rs.getString(7)  ),
+                    new Supplier(rs.getString(8), rs.getString(9)));
+            products.add(product);
         }
-
+        return products;
     }
 
     @Override
-    public List<Product> getBy(Supplier supplier) {
-        return null;
+    public List<Product> getBy(Supplier supplier) throws SQLException {
+        int supplierId = supplier.getId();
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM product " +
+                "INNER JOIN PRODUCT_CATEGORY PC ON PRODUCT.CATEGORY_ID = PC.ID " +
+                "INNER JOIN supplier s ON product.supplier_id = s.id  " +
+                "WHERE supplier_id = ?");
+        ps.setInt(1,supplierId);
+        ResultSet rs = ps.executeQuery();
+        List<Product> products = new ArrayList<>();
+        while (rs.next()){
+            Product product = new Product(
+                    rs.getString(1),
+                    rs.getFloat(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    new ProductCategory(rs.getString(5),
+                            rs.getString(6),rs.getString(7)  ),
+                    new Supplier(rs.getString(8), rs.getString(9)));
+            products.add(product);
+        }
+        return products;
     }
 
     @Override
-    public List<Product> getBy(ProductCategory productCategory) {
-        return null;
+    public List<Product> getBy(ProductCategory productCategory) throws SQLException {
+        int productCategoryId = productCategory.getId();
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM product " +
+                "INNER JOIN PRODUCT_CATEGORY PC ON PRODUCT.CATEGORY_ID = PC.ID " +
+                "INNER JOIN supplier s ON product.supplier_id = s.id  " +
+                "WHERE pc.id = ?");
+        ps.setInt(1,productCategoryId);
+        ResultSet rs = ps.executeQuery();
+        List<Product> products = new ArrayList<>();
+        while (rs.next()){
+            Product product = new Product(
+                    rs.getString(1),
+                    rs.getFloat(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    new ProductCategory(rs.getString(5),
+                            rs.getString(6),rs.getString(7)  ),
+                    new Supplier(rs.getString(8), rs.getString(9)));
+            products.add(product);
+        }
+        return products;
     }
 }

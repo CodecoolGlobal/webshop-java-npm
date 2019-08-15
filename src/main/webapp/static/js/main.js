818e42561a6ca2a48031db9c3f86c50532b5ed21
Joó Norbert <joonorbi12@gmail.com>
2019-08-15 13:26:16 +0200

    addeventlisener added to radio input

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -5,3 +5,14 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+function main(){
+    sort()
+}
+function sort() {
+    let radio = document.getElementById("supID");
+    let prod = document.getElementById("products");
+    radio.addEventListener("click",()=>{
+        console.log(prod.getAttribute("data-prod"));
+    })
+}
+main();
